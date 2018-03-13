424b7f509b2b5d1cbf3d1a55fe0e6e808baf3a71
Jonathan Chaney <jonchaney@gmail.com>
2018-03-13 11:40:18 -0700

    api return ten images working

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-// const http = require('http').Server(app)
 const path = require('path')
 const fetch = require('node-fetch')
 const PORT = 8000
+
+const apiKey = require('./config.js')
 
 app.use(express.static('public'))
 
@@ -13,7 +14,14 @@ app.get('/', (req, res) => {
 });
 
 app.get('/images', (req, res) => {
-  // make fetch request
+  fetch(`https://www.googleapis.com/customsearch/v1?key=${apiKey}&cx=015250315716043265163:wsefhoswelu&searchType=image&imageSize=medium&imgColorType=mono&num=10&q=piano`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((myJson) => {
+    console.log(myJson.items);
+  });
+
 });
 
 app.listen(PORT, () => {
