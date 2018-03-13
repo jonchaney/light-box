d419a33373dd9827fb0278d3d3c3d46533df46b9
Jonathan Chaney <jonchaney@gmail.com>
2018-03-12 23:38:12 -0700

    set up node backend

--- app.js
+++ app.js
@@ -10,22 +10,7 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'))
-})
-
-app.get('/fighters', (req, res) => {
-  let results
-  fetch('http://ufc-data-api.ufc.com//api/v1/us/fighters')
-    .then(function(response) {
-        return response.text();
-    }).then(function(body) {
-      results = JSON.parse(body)
-        console.log(typeof body);
-        console.log(body.length);
-        console.log(JSON.parse(body)[0]);
-        res.send(results)
-    });
-
-})
+});
 
 app.listen(PORT, () => {
   console.log(__dirname);
