b6d913e9634bd518b0a2ffe555cc45661d002dc9
Jonathan Chaney <jonchaney@gmail.com>
2018-03-14 17:24:43 -0700

    lightbox working

--- public/javascripts/lightBox.js
+++ public/javascripts/lightBox.js
@@ -1,10 +1,15 @@
 class LightBox {
     constructor(images) {
         this.images = images;
+        this.currentImageIndex;
     }
 
     render(index) {
+        console.log(index, 'passed in');
+        console.log(this.currentImageIndex, 'current')
+        this.currentImageIndex = index;
         let element = document.getElementById('main') // get main element
+
         let lightBox = document.createElement('div')  // create lightbox
         lightBox.setAttribute('id', 'lightbox');
         
@@ -20,16 +25,22 @@ class LightBox {
         let p = document.createElement('p');
         let x = document.createElement('p');
         p.innerHTML = left;
+        p.addEventListener('click', (event) => this.leftClick(event))
         lightBox.appendChild(p)
         lightBox.appendChild(section)
         x.innerHTML = right;
+        x.addEventListener('click', (event) => this.rightClick(event))
         lightBox.appendChild(x)
         element.appendChild(lightBox);
-
-        
     }
 
+    leftClick(event) {
+        this.render(parseInt(this.currentImageIndex) + this.images.length - 1)
+    }
 
+    rightClick(event) {
+        this.render(parseInt(this.currentImageIndex) + 1)
+    }
 }
 
 module.exports = LightBox;
